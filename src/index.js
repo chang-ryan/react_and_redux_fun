4904162306b3410221c542ef007293d7049cdc2a
Ryan Chang <ry4nch4ng@gmail.com>
2016-11-02 20:50:28 -0700

    start video search

--- src/index.js
+++ src/index.js
@@ -11,21 +11,31 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({ key: API_KEY, term: 'drake' }, (videos) =>
-      this.setState({ videos })
-      // this.setState({ videos: videos })
-      // Only works if key is the same as value, ie. videos: videos
-    );
+    this.videoSearch('drake');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
   }
 
   render() {
     return (
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo: selectedVideo })}
+          videos={this.state.videos} />
       </div>
     );
   }
