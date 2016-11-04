f1fb17ff4648afc49244d9463d3d1050ac40f160
Ryan Chang <ry4nch4ng@gmail.com>
2016-11-04 10:51:27 -0700

    redux connect in book_list

---
+++ redux_part2/src/containers/book_list.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookList extends Component {
+  renderList() {
+    return this.props.books.map((book) => {
+      return (
+        <li key={book.title} className="list-group-item">
+          {book.title}
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  // Whatever this function returns will be accessible via this.props in BookList
+  return {
+    books: state.books
+    // Will be accessible via this.props.books (see renderList() from above)
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
---
+++ redux_part2/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+function() {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Harry Potter' },
+    { title: 'Ego Is The Enemy' },
+    { title: 'Eloquent Ruby' }
+  ];
+}
