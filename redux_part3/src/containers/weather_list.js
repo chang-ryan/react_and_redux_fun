aaa8c8365ebfe98f60d3cb3245f8caf0acc24651
Ryan Chang <ry4nch4ng@gmail.com>
2016-11-09 15:05:36 -0800

    finish google maps

--- redux_part3/src/containers/weather_list.js
+++ redux_part3/src/containers/weather_list.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Sparklines, SparklinesLine } from 'react-sparklines';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 
 class WeatherList extends Component {
@@ -10,12 +11,15 @@ class WeatherList extends Component {
     const temps = cityData.list.map(item => item.main.temp)
     const pressures = cityData.list.map(item => item.main.pressure)
     const humidities = cityData.list.map(item => item.main.humidity)
+    // const lon = cityData.city.coord.lon;
+    // const lat = cityData.city.coord.lat;
+    const { lon, lat } = cityData.city.coord;
 
     return (
       // the rule for adding a key in react:
       // add it to the top element
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td><Chart data={temps} color="red" units="K" /></td>
         <td><Chart data={pressures} color="purple" units="hPa" /></td>
         <td><Chart data={humidities} color="blue" units="%" /></td>
