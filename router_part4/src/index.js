18d7f10765928cda56fff30ce9dbdbc7c4c2b5b9
Ryan Chang <ry4nch4ng@gmail.com>
2016-11-11 19:12:57 -0800

    link to posts#new

--- router_part4/src/index.js
+++ router_part4/src/index.js
@@ -4,13 +4,16 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
 // browserHistory tells us which part of the URL react actually cares about
-
+import promise from 'redux-promise';
+import routes from './routes';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(
+  promise
+)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory} />
+    <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container'));
