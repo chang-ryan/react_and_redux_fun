3f39f99996d62869a03f898136b251a937a23087
Ryan Chang <ry4nch4ng@gmail.com>
2016-11-15 10:28:41 -0800

    refactor posts_new with lodash

--- router_part4/src/components/posts_new.js
+++ router_part4/src/components/posts_new.js
@@ -1,9 +1,24 @@
+import _ from 'lodash';
 import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { Link } from 'react-router';
 
 import { createPost } from '../actions/index';
 
+const FIELDS = {
+  title: {
+    type: 'input',
+    label: 'Title for Post'
+  },
+  categories: {
+    type: 'input',
+    label: 'Enter some categories for this post'
+  },
+  content: {
+    type: 'textarea',
+    label: 'Post Content'
+  }
+}; // ['title', 'categories', 'content'];
 
 class PostsNew extends Component {
   static contextTypes = {
@@ -18,36 +33,32 @@ class PostsNew extends Component {
       });
   }
 
+  renderField(fieldConfig, field) {
+    const fieldHelper = this.props.fields[field];
+
+    return (
+      <div
+        key={field} 
+        className={`form-group ${fieldHelper.touched && fieldHelper.invalid ? 'text-danger' : ''}`}>
+        <label>{fieldConfig.label}</label>
+        <fieldConfig.type type="text" className="form-control" {...fieldHelper} />
+        <div className="text-help">
+          {fieldHelper.touched ? fieldHelper.error : ''}
+        </div>
+      </div>
+    );
+  }
+
   render() {
     // const handleSubmit = this.props.handleSubmit;
     const { handleSubmit } = this.props; // ES6 syntax identical to above
     // const title = this.props.fields.title;
-    const { fields: { title, categories, content }} = this.props;
+    const { fields: { title, categories, content }} = this.props; // ES6 syntax identical to above
 
     return (
       <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create a new post</h3>
-        <div className={`form-group ${title.touched && title.invalid ? 'text-danger' : ''}`}>
-          <label>Title</label>
-          <input type="text" className="form-control" {...title} />
-          <div className="text-help">
-            {title.touched ? title.error : ''}
-          </div>
-        </div>
-        <div className={`form-group ${categories.touched && categories.invalid ? 'text-danger' : ''}`}>
-          <label>Categories</label>
-          <input type="text" className="form-control" {...categories} />
-          <div className="text-help">
-            {categories.touched ? categories.error : ''}
-          </div>
-        </div>
-        <div className={`form-group ${content.touched && content.invalid ? 'text-danger' : ''}`}>
-          <label>Content</label>
-          <textarea className="form-control" {...content} />
-          <div className="text-help">
-            {content.touched ? content.error : ''}
-          </div>
-        </div>
+        {_.map(FIELDS, this.renderField.bind(this))}
         <button type="submit" className="btn btn-primary">Submit</button>
         <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
@@ -58,15 +69,11 @@ class PostsNew extends Component {
 function validate(values) {
   const errors = {};
 
-  if (!values.title) {
-    errors.title = 'Enter a title';
-  }
-  if (!values.categories) {
-    errors.categories = 'Enter some categories';
-  }
-  if (!values.content) {
-    errors.content = 'Enter some content';
-  }
+  _.each(FIELDS, (type, field) => {
+    if (!values[field]) {
+      errors[field] = `Please enter ${field}`;
+    }
+  });
 
   return errors;
 }
@@ -76,7 +83,7 @@ function validate(values) {
 
 export default reduxForm({
   form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content'],
+  fields: _.keys(FIELDS),
   // validate: validate
   validate // ES6 syntax for the above statement
 }, null, { createPost })(PostsNew);
