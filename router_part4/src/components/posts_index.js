e27a824968c2b6d0984a9dba635e1ebb9430ec29
Ryan Chang <ry4nch4ng@gmail.com>
2016-11-27 16:54:02 -0800

    update posts_index

--- router_part4/src/components/posts_index.js
+++ router_part4/src/components/posts_index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { fetchPosts } from '../actions/index';
 import { Link } from 'react-router';
+
+import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
   componentWillMount() {
@@ -19,7 +20,7 @@ class PostsIndex extends Component {
           </Link>
         </li>
       );
-    })
+    });
   }
 
   render() {
